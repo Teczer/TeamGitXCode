e7ed483371814338649be71d73c9626651c9ee0d
Anne Ferret <anneferret94@gmail.com>
2022-06-16 14:01:35 +0200

    add text anne

--- TeamGitXCode/ContentView.swift
+++ TeamGitXCode/ContentView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            Text("L'équipe est...")
+            Text("Anne")
             Text("Mehdi")
         }
     }
