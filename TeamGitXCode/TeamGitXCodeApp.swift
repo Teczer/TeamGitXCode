012e2d84503c8c26b2e436d1d34dac7e204c2a6d
Mehdi <mehdi.hattou1@gmail.com>
2022-06-16 13:42:52 +0200

    Initial Commit

---
+++ TeamGitXCode/TeamGitXCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamGitXCodeApp.swift
+//  TeamGitXCode
+//
+//  Created by Apprenant 67 on 16/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct TeamGitXCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
